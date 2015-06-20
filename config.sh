04d57a0fdc4811697c43f5eeb0a376b24c4407d8
Knight <Knight42@mail.ustc.edu.cn>
2015-06-20 16:23:01 +0800

    increase capacity

--- config.sh
+++ config.sh
@@ -3,7 +3,8 @@ LOGDIR=/var/log/ssd-cache-log
 WWWROOT=/srv/www
 CACHEROOT=/mnt/ssd/cache
 CACHETMPDIR=/mnt/ssd/tmp
-cachesize=$((1000*1000*1000*235)) # leave 4G save space
+#cachesize=$((1000*1000*1000*235)) # leave 4G save space
+cachesize=$((1000*1000*1000*238)) # leave 4G save space
 
 today=$(date +'%Y%m%d')
 
