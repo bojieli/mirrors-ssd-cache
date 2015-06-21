238ccf531be163988c379b85545cc649ac64e610
Knight <Knight42@mail.ustc.edu.cn>
2015-06-21 12:17:35 +0800

    extend capacity slightly

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ WWWROOT=/srv/www
 CACHEROOT=/mnt/ssd/cache
 CACHETMPDIR=/mnt/ssd/tmp
 #cachesize=$((1000*1000*1000*235)) # leave 4G save space
-cachesize=$((1000*1000*1000*238)) # leave 4G save space
+cachesize=$((1000*1000*1000*240)) # leave 4G save space
 
 today=$(date +'%Y%m%d')
 
