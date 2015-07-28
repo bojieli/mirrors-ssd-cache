582dff3860f76659505140cf88c7cde4da78ca24
Knight <Knight42@mail.ustc.edu.cn>
2015-07-28 10:24:55 +0800

    minor change

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ LOGDIR=/var/log/ssd-cache-log
 WWWROOT=/srv/www
 CACHEROOT=/mnt/ssd/cache
 CACHETMPDIR=/mnt/ssd/tmp
-cachesize=$((1024*1024*1024*225)) # leave 4G save space
+declare -i cachesize=$((225*1024**3)) # leave 4G save space
 
 today=$(date +'%Y%m%d')
 
