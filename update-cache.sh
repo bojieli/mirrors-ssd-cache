7310ec5f42ef379838df29962826d96faca89276
Knight <Knight42@mail.ustc.edu.cn>
2015-08-09 10:46:59 +0800

    bug fixed

--- update-cache.sh
+++ update-cache.sh
@@ -31,9 +31,10 @@ if [ -z "$mirror" ]; then # full update
     sync_from_file_list $tocache_list
 else # update one mirror
     tmpfile=$(mktemp /tmp/mirror.XXXXXXX)
+
     trap "rm -f $tmpfile" EXIT
-    grep "^/$mirror" $tocache_list | cut -c $((${#mirror}+2))- >$tmpfile
+
+    grep "^/$mirror/" $tocache_list | cut -c $((${#mirror}+2))- >$tmpfile
     sync_from_file_list $tmpfile
-    #rm -f $tmpfile
 fi
 
